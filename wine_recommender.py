9ac0df8f509f00c6dfcd99d00585c67fcf5b2fe3
seanyew25 <seanyew25@gmail.com>
2025-04-03 00:30:26 +0800

    Fix streamlit sqlite error

--- wine_recommender.py
+++ wine_recommender.py
@@ -4,16 +4,9 @@ import chromadb
 from chromadb.utils import embedding_functions
 from dotenv import load_dotenv
 import os
-__import__('pysqlite3')
+import('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 # Load environment variables
 load_dotenv()
 gemini_api_key = os.getenv("GEMINI_API_KEY")
